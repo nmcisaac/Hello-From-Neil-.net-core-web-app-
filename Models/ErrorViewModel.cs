f095e440047a03a095853ce54d6aced33da6edcc
Neil McIsaac (hotmail) <nmcisaac@hotmail.com>
2020-07-20 04:00:00 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloFromNeil.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
